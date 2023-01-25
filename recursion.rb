dee1fe4f4b96561c23c2be92bb597e3f8c682eff
Kelly Zhou <kellyzhou@Kellys-Macbook.local>
2023-01-25 14:16:30 -0500

    sum array

--- recursion.rb
+++ recursion.rb
@@ -2,3 +2,12 @@ def range(start, ender)
     return [] if ender <= start
     [start] + range(start+1, ender)
 end
+
+def sum_arr(arr)
+    return 0 if arr.length == 0 || !arr.is_a?(Array)
+    arr.shift + sum_arr(arr)
+end
+
+def iter_arr(arr)
+    arr.inject { |acc, ele| acc + ele }
+end
